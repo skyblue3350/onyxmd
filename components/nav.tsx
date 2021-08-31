693ff78921fdf8d98062217ca811819ff01655e0
sky <skyblue3350@gmail.com>
2021-09-01 08:56:09 +0900

    fix nav design

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,34 +1,32 @@
 import Link from 'next/link'
 import React from 'react'
-import { Container, Icon, Label, Menu } from 'semantic-ui-react'
+import { Icon, Label, Menu } from 'semantic-ui-react'
 
 const Nav = () => {
     return (
         <Menu fixed='top' inverted icon>
-            <Container>
-                <Menu.Item>
-                    <Link href='/'>onyxmd</Link>
-                </Menu.Item>
-                <Menu.Item fitted>
-                    <Menu icon inverted>
-                        <Menu.Item>
-                            <Icon inverted name='pencil' />
-                        </Menu.Item>
-                        <Menu.Item>
-                            <Icon inverted name='columns' />
-                        </Menu.Item>
-                        <Menu.Item>
-                            <Icon inverted name='eye' />
-                        </Menu.Item>
-                    </Menu>
-                </Menu.Item>
-                <Menu.Item position='right'>
-                    <Label color='blue' size='large'>
-                        <Icon name='users' />
-                        1 Online
-                    </Label>
-                </Menu.Item>
-            </Container>
+            <Menu.Item position='left'>
+                <Link href='/'>onyxmd</Link>
+            </Menu.Item>
+            <Menu.Item fitted>
+                <Menu icon inverted>
+                    <Menu.Item>
+                        <Icon inverted name='pencil' />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Icon inverted name='columns' />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Icon inverted name='eye' />
+                    </Menu.Item>
+                </Menu>
+            </Menu.Item>
+            <Menu.Item position='right'>
+                <Label color='blue' size='large'>
+                    <Icon name='users' />
+                    1 Online
+                </Label>
+            </Menu.Item>
         </Menu>
     )
 }
