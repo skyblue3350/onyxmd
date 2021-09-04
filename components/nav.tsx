5ec74f2ff42f28380afbfee0af837599e5db2caa
sky <skyblue3350@gmail.com>
2021-09-05 03:36:24 +0900

    update nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,21 +2,28 @@ import Link from 'next/link'
 import React from 'react'
 import { Icon, Label, Menu } from 'semantic-ui-react'
 
-const Nav = () => {
+interface Props {
+    onClickEdit: () => void
+    onClickBoth: () => void
+    onClickView: () => void
+    mode: string
+}
+
+const Nav = (props: Props) => {
     return (
-        <Menu fixed='top' inverted icon>
+        <Menu inverted icon style={{margin: 0}}>
             <Menu.Item position='left'>
                 <Link href='/'>onyxmd</Link>
             </Menu.Item>
             <Menu.Item fitted>
                 <Menu icon inverted>
-                    <Menu.Item>
+                    <Menu.Item active={props.mode==='edit'} onClick={props.onClickEdit}>
                         <Icon inverted name='pencil' />
                     </Menu.Item>
-                    <Menu.Item>
+                    <Menu.Item active={props.mode==='both'} onClick={props.onClickBoth}>
                         <Icon inverted name='columns' />
                     </Menu.Item>
-                    <Menu.Item>
+                    <Menu.Item active={props.mode==='view'} onClick={props.onClickView}>
                         <Icon inverted name='eye' />
                     </Menu.Item>
                 </Menu>
--- pages/notes/[id].tsx
+++ pages/notes/[id].tsx
@@ -14,16 +14,19 @@ const Page = () => {
   const router = useRouter()
   const [markdown, setMarkdown] = useState('')
   const [revision, setRevision] = useState(0)
+  const [noteId, setNoteId] = useState('')
+  const [mode, setMode] = useState('edit')
   const socketRef = useRef(null)
 
   useEffect(() => {
     if (router.isReady) {
-      const noteId = router.query.id
+      setNoteId(router.query.id.toString())
+      router.query.mode && setMode(router.query.mode.toString())
       socketRef.current = SocketIOClient()
-      
+
       const socket = socketRef.current
 
-      socket.emit('join', noteId)
+      socket.emit('join', router.query.id)
       socket.on('doc', (r, md) => {
         setRevision(r)
         setMarkdown(md)
@@ -43,24 +46,36 @@ const Page = () => {
     }
   }, [router, ])
 
+  const changeMode = (mode: 'edit' | 'both' | 'view') => {
+    router.push({pathname: '', query: {mode: mode, id: noteId}}, '', {shallow: true})
+    setMode(mode)
+  }
+
   return (
     <>
-      <Nav />
-      <Grid columns={2} style={{paddingTop: 45}}>
+      <Nav mode={mode}
+        onClickEdit={() => changeMode('edit')}
+        onClickBoth={() => changeMode('both')}
+        onClickView={() => changeMode('view')} />
+      <Grid columns={mode === 'both'? 2 : 1}>
+        {mode === 'edit' || mode === 'both' ?
         <Grid.Column>
-        <AceEdit
-          style={{width: '100%', height: '100vw'}}
-          value={markdown}
-          onChange={(value, e) => {
-            socketRef.current.emit(e.action, revision, e)
-            setMarkdown(value)
-            setRevision((prevrevision) => prevrevision+1)
-          }}
-          setOptions={{useWorker: false}} />
+          <AceEdit
+            style={{width: '100%', height: '100vw'}}
+            value={markdown}
+            onChange={(value, e) => {
+              socketRef.current.emit(e.action, revision, e)
+              setMarkdown(value)
+              setRevision((prevrevision) => prevrevision+1)
+            }}
+            setOptions={{useWorker: false}} />
         </Grid.Column>
+        : null}
+        {mode === 'view' || mode === 'both' ?
         <Grid.Column>
           <Markdown markdown={markdown}/>
         </Grid.Column>
+        : null}
       </Grid>
       
     </>
