a92aca14593633324e084760328cdcebcd79b637
sky <skyblue3350@gmail.com>
2021-09-06 01:18:19 +0900

    add markdown syntax highlight

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MarkdownIt from 'markdown-it'
 import MarkdownItAnchor from 'markdown-it-anchor'
 import MarkdownItTableOfContents from 'markdown-it-table-of-contents'
+import hljs from 'highlight.js'
 
 interface Props {
     markdown: string
@@ -11,6 +12,16 @@ const Markdown = (props: Props) => {
     const md = MarkdownIt({
         breaks: true,
         linkify: true,
+        highlight: (code, lang) => {
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return '<pre class="hljs"><code>' + hljs.highlight(code, {language: lang}).value + '</code></pre>'
+                } catch {
+                    return '<pre class="hljs"><code>' + MarkdownIt().utils.escapeHtml(code) + '</code></pre>'
+                }
+            }
+            return '<pre class="hljs"><code>' + MarkdownIt().utils.escapeHtml(code) + '</code></pre>'
+        },
     })
     md.use(MarkdownItAnchor)
     md.use(MarkdownItTableOfContents)
