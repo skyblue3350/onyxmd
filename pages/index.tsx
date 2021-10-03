d219caa846901ea59ceeb2d5305e99157cc17747
sky <skyblue3350@gmail.com>
2021-10-04 01:38:36 +0900

    add demo page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,28 @@
-import { Button } from 'semantic-ui-react'
-import socket from 'socket.io-client'
+import router from "next/router"
+import React, { useState } from "react"
+import { Container, Form, Header, Segment } from "semantic-ui-react"
 
 export default function Page() {
-  const clinet = socket()
+  const [room, setRoom] = useState('')
 
-  return <>
-      <Button as='a' href='/github'>github</Button>
-  </>
+  return <Container>
+    <Segment>
+      <Header content='TopPage' />
+      <Form onSubmit={() => {
+        if (room.length > 0) {
+          router.push(`/notes/${room}`)
+        }
+      }}>
+        <Form.Group>
+          <Form.Input
+            placeholder='room id'
+            value={room}
+            onChange={(event, data) => setRoom(data.value.toString())} />
+          <Form.Button
+            content='join'
+            submit />
+        </Form.Group>
+      </Form>
+    </Segment>
+  </Container>
 }
