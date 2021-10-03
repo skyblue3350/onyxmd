baabd1c217bd218956ffe40603d33f7ded3ec43e
sky <skyblue3350@gmail.com>
2021-10-04 01:18:16 +0900

    add codemirror css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
 import 'highlight.js/styles/github-dark.css'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/theme/idea.css'
+import 'codemirror/theme/monokai.css'
 
 function MyApp({ Component}) {
   return <Component />
