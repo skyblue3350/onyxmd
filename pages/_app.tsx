c401db289b143260ad5493df913ee313377ca2a5
sky <skyblue3350@gmail.com>
2021-10-21 03:06:45 +0900

    add next-auth provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import { Provider } from 'next-auth/client'
+
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
 import 'highlight.js/styles/github-dark.css'
@@ -6,8 +8,8 @@ import 'codemirror/theme/material.css'
 import 'codemirror/theme/idea.css'
 import 'codemirror/theme/monokai.css'
 
-function MyApp({ Component}) {
-  return <Component />
+function MyApp({Component, pageProps}) {
+  return <Provider session={pageProps.session}><Component {...pageProps} /></Provider>
 }
 
 export default MyApp
