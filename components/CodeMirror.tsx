41e26cdd5705f458b6d72f648b0de95d13f6f98f
sky <skyblue3350@gmail.com>
2022-04-11 01:41:43 +0900

    replace codemirror6

--- components/CodeMirror.tsx
+++ components/CodeMirror.tsx
@@ -1,14 +1,21 @@
-import { useEffect, useState } from 'react'
-import { CodemirrorBinding } from 'y-codemirror'
+import { useEffect, useRef, useState } from 'react'
 import { WebsocketProvider } from 'y-websocket'
 import * as Y from 'yjs'
-import { UnControlled as CodeMirrorEditor } from "react-codemirror2"
-import 'codemirror/mode/markdown/markdown'
-import 'codemirror/keymap/vim'
-import 'codemirror/keymap/sublime'
-import 'codemirror/keymap/emacs'
+import { EditorState } from '@codemirror/state'
+import { EditorView, keymap } from '@codemirror/view'
 import { Menu, Dropdown, Input } from 'semantic-ui-react'
 import { useSession } from 'next-auth/react'
+import { yCollab } from 'y-codemirror.next'
+import { highlightActiveLineGutter, lineNumbers } from '@codemirror/gutter'
+import { markdown } from '@codemirror/lang-markdown'
+import { closeBrackets } from '@codemirror/closebrackets'
+import { history, historyKeymap } from '@codemirror/history'
+import { defaultHighlightStyle, classHighlightStyle } from '@codemirror/highlight'
+import { oneDarkTheme } from '@codemirror/theme-one-dark'
+import { autocompletion, CompletionContext } from '@codemirror/autocomplete'
+import { rectangularSelection } from '@codemirror/rectangular-selection'
+import { emojiEmotion } from 'emoji-emotion'
+import { indentWithTab } from '@codemirror/commands'
 
 interface Awareness {
   [key:string]: any
@@ -28,8 +35,7 @@ const CodeMirror = (props: Props) => {
   const keyMaps = ['sublime', 'vim', 'emacs']
   const themes = ['material', 'idea', 'monokai']
 
-  const [EditorRef, setEditorRef] = useState(null)
-  const [code, setCode] = useState('')
+  const editorRef = useRef()
   const [keyMap, setKeyMap] = useState(localStorage.getItem('keymap') || keyMaps[0])
   const [theme, setTheme] = useState(localStorage.getItem('theme') || themes[0])
   const [tabSize, setTabSize] = useState(parseInt(localStorage.getItem('tabSize') || '2'))
@@ -41,24 +47,60 @@ const CodeMirror = (props: Props) => {
   }, [keyMap, theme, tabSize])
 
   useEffect(() => {
-    if (EditorRef !== null && !session) {
+    if (editorRef.current && !session) {
       const ydoc = new Y.Doc()
       const wsProvider = new WebsocketProvider(`ws://${location.host}`, props.room, ydoc)
       const yText = ydoc.getText('codemirror')
-      const yUndoManager = new Y.UndoManager(yText)
-      const awareness = wsProvider.awareness
-      awareness.setLocalStateField('user', {
+      wsProvider.awareness.setLocalStateField('user', {
         name: session ? session.user.name : props.username,
         color: props.color,
         icon: session ? session.user.image : undefined,
       })
   
-      const getBinding = new CodemirrorBinding(yText, EditorRef, awareness, {
-        yUndoManager,
+      const editor = new EditorView({
+        state: EditorState.create({
+          doc:yText.toString(),
+          extensions: [
+            defaultHighlightStyle,
+            classHighlightStyle,
+            EditorState.allowMultipleSelections.of(true),
+            oneDarkTheme,
+            history(),
+            closeBrackets(),
+            lineNumbers(),
+            highlightActiveLineGutter(),
+            rectangularSelection(),
+            markdown(),
+            keymap.of([
+              indentWithTab,
+              ...historyKeymap,
+            ]),
+            yCollab(yText, wsProvider.awareness, {undoManager: false}),
+            EditorView.updateListener.of((update) => {
+              update.docChanged && props.onChange && props.onChange(update.state.doc.toString())
+            }),
+            autocompletion({
+              override: [(context: CompletionContext) => {
+                let word = context.matchBefore(/[:\w]*/)
+                if (word.from == word.to && !context.explicit)
+                  return null
+                return {
+                  from: word.from,
+                  options: emojiEmotion.map(e => ({
+                    label: `:${e.name}:`,
+                    type: 'keyword',
+                    info: e.emoji
+                  }))
+                }
+              }]
+            })
+          ]
+        }),
+        parent: editorRef.current
       })
 
-      awareness.on('change', () => {
-        props.onUserChange && props.onUserChange(awareness.getStates())
+      wsProvider.awareness.on('change', () => {
+        props.onUserChange && props.onUserChange(wsProvider.awareness.getStates())
       })
 
       wsProvider.on('status', ({status}) => {
@@ -66,34 +108,16 @@ const CodeMirror = (props: Props) => {
       })
 
       return () => {
-        if (wsProvider.wsconnected) {
-          wsProvider.disconnect()
-          ydoc.destroy()
-        }
+        editor.destroy()
       }
     }
-  }, [EditorRef, props, session, session])
+  }, [editorRef, props, session])
 
-return <>
-      <CodeMirrorEditor
-          className='CodeMirror-css'
-          options={{
-            mode: 'markdown',
-            theme: theme,
-            keyMap: keyMap,
-            lineNumbers: true,
-            lineWrapping: true,
-            tabSize: tabSize,
-          }}
-          onChange={(editor, data, value) => {
-            setCode(value);
-            props.onChange && props.onChange(value)
-          }}
-          editorDidMount={(editor) => {
-            setEditorRef(editor)
-          }}
-       />
-       <Menu className='editor-menu' inverted>
+return (
+    <>
+      <div className='CodeMirror-css'
+           ref={editorRef} />
+      <Menu className='editor-menu' inverted>
         <Dropdown item text={keyMap} style={{width: 100}}>
           <Dropdown.Menu>
             {keyMaps.map(km => {
@@ -111,8 +135,8 @@ return <>
         <Menu.Item>
           <Input type='number' min={1} max={10} label='tab size' value={tabSize} onChange={(e, data) => setTabSize(parseInt(data.value))} />
         </Menu.Item>
-       </Menu>
-    </>
+      </Menu>
+    </>)
 }
 
 export default CodeMirror
